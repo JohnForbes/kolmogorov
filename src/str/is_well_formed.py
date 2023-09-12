c56ab5bed401a32c031f3a2e5a8d1759c0b98b56
John Forbes <john.robert.forbes@gmail.com>
2023-09-13 09:56:52 +1000

    Added src/str/is_well_formed.py.

---
+++ src/str/is_well_formed.py
@@ -0,0 +1,35 @@
+from hak.pxyf import f as pxyf
+from hak.pf import f as pf
+
+from src.dict.string_and_char_pairs.char_pair_frequencies_equal import f as cpfe
+from src.dict.string_and_char.string_has_even_char_freq import f as e
+
+# filter
+# is_well_formed
+f = lambda x: all([
+  'x' in x,
+  x[ 0] not in ' )}]',
+  x[-1] not in ' ({[',
+  e({'string': x, 'char': '"'}),
+  e({'string': x, 'char': "'"}),
+  cpfe({'string': x, 'char_pairs': [("(", ")"), ("{", "}"), ("[", "]")]})
+])
+
+def t():
+  if not (lambda: pxyf('abc', 0, f))(): return pf('!t_0_no_x')
+  if not (lambda: pxyf('axc', 1, f))(): return pf('!t_1_x')
+  if not (lambda: pxyf(' x...', 0, f))(): return pf('!t_0_a')
+  if not (lambda: pxyf(')x...', 0, f))(): return pf('!t_0_b')
+  if not (lambda: pxyf('}x...', 0, f))(): return pf('!t_0_c')
+  if not (lambda: pxyf(']x...', 0, f))(): return pf('!t_0_d')
+  if not (lambda: pxyf('x... ', 0, f))(): return pf('!t_0_e')
+  if not (lambda: pxyf('x...(', 0, f))(): return pf('!t_0_f')
+  if not (lambda: pxyf('x...{', 0, f))(): return pf('!t_0_g')
+  if not (lambda: pxyf('x...[', 0, f))(): return pf('!t_0_h')
+  if not (lambda: pxyf("x'..'", 1, f))(): return pf('!t_1_i')
+  if not (lambda: pxyf("x'.''", 0, f))(): return pf('!t_0_i')
+  if not (lambda: pxyf('x".."', 1, f))(): return pf('!t_1_j')
+  if not (lambda: pxyf('x".""', 0, f))(): return pf('!t_0_j')
+  if not (lambda: pxyf('(){}[]]x', 0, f))(): return pf('!t_0_k')
+  if not (lambda: pxyf('(){}[]x', 1, f))(): return pf('!t_1_k')
+  return 1
