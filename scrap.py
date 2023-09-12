89a13097aba2a16806c82f30c0df7f1774bce866
John Forbes <john.robert.forbes@gmail.com>
2023-09-12 19:50:21 +1000

    Added scrap.py.

---
+++ scrap.py
@@ -0,0 +1,15 @@
+"""
+from random import random as u
+do_print = u() > 1 - 0.001
+
+if do_print:
+  print(f'def t_a():')
+  print(_)
+
+if do_print:
+  print(_)
+  print('  z = f(**x)')
+  print('  return pxyz(x, y, z)')
+  print()
+
+"""
