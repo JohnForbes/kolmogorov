2d63b83cb16aa18e98cd2d9058c1817fa56de474
John Forbes <john.robert.forbes@gmail.com>
2023-09-13 09:56:41 +1000

    Updated src/main.py.

--- src/main.py
+++ src/main.py
@@ -1,14 +1,14 @@
 from hak.pf import f as pf
 
 from src.str.to_next_str import f as to_next_str
-from src.filter import f as filter
+from src.str.is_well_formed import f as is_well_formed
 
 def f(x):
   string = ' '
   functions = []
   while len(string)<(x-1):
     string = to_next_str(string)
-    if filter(string):
+    if is_well_formed(string):
       functions.append(string)
   return functions
 
